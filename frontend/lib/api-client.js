b9dbc8301e3cb6258406e8b2291ccc5e9143ad91
Mukesh Kumar <Mukeshkumar@cloudprism.in>
2025-01-14 12:03:16 +0530

    minor fix

--- frontend/lib/api-client.js
+++ frontend/lib/api-client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+const baseURL =
+  process.env.NEXT_PUBLIC_BASE_URL || "https://api.codingcommando.in/api/v1";
 
 if (!baseURL) {
   console.error("NEXT_PUBLIC_BASE_URL is not defined");
